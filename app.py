cf4089cb3f72e6d9eea300c8bfcc91016e25a1e9
Adam J <adamjac@kth.se>
2018-02-21 13:08:57 +0100

    finished booking deletion

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ def employees():
   cursor.execute('SELECT p.person_id, p.full_name FROM Person p INNER JOIN '\
   'Employee e ON p.person_id = e.person_id')
   emps = cursor.fetchall()  
-  print(emps)
   return render_template('employees.html', employees=emps) 
 
 # See your meetings and possibly cancel some
@@ -28,20 +27,46 @@ def meetings(id):
     # error message
     error_message = ""
     
-    # post request: delete a meeting 
-    #if request.method == 'POST':
-    #  meeting_id = request.form['meeting_id']
+    # post request: delete meetings
+    if request.method == 'POST':
+      global cursor
+      b_ids = request.form.getlist('booking_id')
+      num_bookings = len(b_ids)
+      p_id = request.form['person_id']
+      b_ids = tuple([int(x) for x in b_ids])
+      cursor.execute("SELECT booking_id FROM Booking b WHERE "\
+      "b.person_id = %s AND b.booking_id IN %s",(int(p_id),b_ids))
+      valid_bids = cursor.fetchall()
+      if num_bookings != len(valid_bids):
+        error_message = "You can only delete bookings made by you!"
+      if len(valid_bids) > 0:  
+        # transform to tuple for the sql query
+        if len(valid_bids) == 1:
+          b_id = valid_bids[0][0]
+          cursor.execute("DELETE FROM Booking b WHERE "\
+          "b.booking_id = %s", [b_id])
+        else:
+          valid_bids = tuple(map(lambda tup: tup[0], valid_bids))
+          cursor.execute("DELETE FROM Booking b WHERE b.booking_id "\
+          "IN %s",(valid_bids))
 
-    # Get meetings and full name 
-    cursor.execute("SELECT full_name FROM Person WHERE person_id=%s", (str(id)))
-    full_name = cursor.fetchone()[0]
-    cursor.execute("SELECT b.start, b.finish, r.name FROM Booking b "\
-    "INNER JOIN Participant p ON p.person_id = %s INNER JOIN Room r ON "\
-    "r.room_id = b.room_id",(str(id)))
+    # Get person_id and full name 
+    cursor.execute("SELECT person_id,full_name FROM Person "\
+    "WHERE person_id=%s", (str(id)))
+    person = cursor.fetchone()
+    person_id  = person[0]
+    full_name = person[1]
+
+    # get bookings
+    cursor.execute("SELECT b.booking_id, b.start, b.finish, r.name FROM "\
+    "Booking b INNER JOIN Participant p ON p.person_id = %s INNER JOIN "\
+    "Room r ON r.room_id = b.room_id",(str(id)))
     meetings = cursor.fetchall()
+
     return render_template(
       'meetings.html', 
       full_name=full_name, 
+      person_id=person_id,
       meetings=meetings,
       error_message=error_message
     )
